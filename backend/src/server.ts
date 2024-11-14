69d86dcf3dbb2ba2560d6d04e9fd7f1122c6c6f7
alan-a-santos <alan-a@uol.com.br>
2024-11-13 23:51:57 -0300

    ajuste de portas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ server.register(routes)
 
 const start = async (): Promise<void> => {
     try {
-      const port = Number(process.env.PORT) || 3001;  
+      const port = Number(process.env.PORT) || 8080;  
       await server.listen({ port, host: '0.0.0.0' });
       console.log('Servidor Conectado');
     } catch (err) {
